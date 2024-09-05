e5372c8814ec586d2ad90ae74670a082b0aaf7b0
Alvinkimutai <alvinchepkiyeng@gmail.com>
2024-09-05 08:56:55 +0300

    adds bark and sit methods

--- lib/dog.py
+++ lib/dog.py
@@ -3,5 +3,16 @@
 class Dog:
     # Class body goes here
 
+    def bark(self):
+        print("Woof!")
+
+
+    def sit(self):
+        print("The dog is sitting.")
+
     #Instance method definition
     pass
+
+barking = Dog.bark(8)
+sit = Dog.sit(3)
+# print(barking)
